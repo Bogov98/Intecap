0c313e38a126b477fc2274d9f2cf9d79774785bf
Carlos Orozco <rebeca.maradiaga@claro.com.gt>
2023-10-19 22:52:56 -0600

    Crud Cursos
    cursos crud

--- sistema/usuario/urls.py
+++ sistema/usuario/urls.py
@@ -10,6 +10,16 @@ urlpatterns = [
     #path('register/', register, name='register'),  
    # path('inicio', views.inicio, name='index'),
     # Otras rutas y configuraciones de URL
+<<<<<<< Updated upstream
     path('inicio', views.inicio, name='inicio'),
+=======
+    path('inicio/', views.inicio, name='inicio'),
+    path('create_curso_view/', views.create_curso_view, name='create_curso_view'),
+    path('create_curso/', views.create_curso, name='create_curso'),
+    path('curso_view/', views.curso_view, name='curso_view'),
+    path('editar_curso_view/<int:idcurso>/', views.editar_curso_view, name='editar_curso_view'),
+    path('editar_curso/<int:idcurso>/', views.editar_curso, name='editar_curso'),
+    path('eliminar_curso/<int:idcurso>/', views.eliminar_curso, name='eliminar_curso'),
+>>>>>>> Stashed changes
     # Otras rutas y configuraciones de URL
 ]
