fbf26f4d20b674836ffb342dcda570c5cba8f646
mynorgc79 <mynorguarchaj99@gmail.com>
2023-10-20 21:57:46 -0600

    cambios sincronizados

--- sistema/usuario/urls.py
+++ sistema/usuario/urls.py
@@ -10,14 +10,9 @@ urlpatterns = [
     #path('register/', register, name='register'),  
    # path('inicio', views.inicio, name='index'),
     # Otras rutas y configuraciones de URL
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
     path('inicio', views.inicio, name='inicio'),
-=======
-<<<<<<< Updated upstream
->>>>>>> 169983e25eaefb433cfe208c447b05903f0ae5b6
+
     path('inicio', views.inicio, name='inicio'),
 
     path('inicio/', views.inicio, name='inicio'),
@@ -27,11 +22,6 @@ urlpatterns = [
     path('editar_curso_view/<int:idcurso>/', views.editar_curso_view, name='editar_curso_view'),
     path('editar_curso/<int:idcurso>/', views.editar_curso, name='editar_curso'),
     path('eliminar_curso/<int:idcurso>/', views.eliminar_curso, name='eliminar_curso'),
-<<<<<<< HEAD
 
-=======
->>>>>>> Stashed changes
->>>>>>> 0c313e38a126b477fc2274d9f2cf9d79774785bf
->>>>>>> 169983e25eaefb433cfe208c447b05903f0ae5b6
     # Otras rutas y configuraciones de URL
 ]
